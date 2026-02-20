d10d57f4fd7a00f58a73f3ef6084e7811811a539
Josué Juca <josuejuca19@gmail.com>
2026-02-20 16:08:23 -0300

    [20/02] Regulamento ajustado

--- src/components/Regulamento/index.tsx
+++ src/components/Regulamento/index.tsx
@@ -13,25 +13,24 @@ export function Regulamento() {
 
         <div className={styles.block}>
           <p className={styles.text}>
-            Ajude-nos a melhorar nossos serviços respondendo a pesquisa de
-            satisfação, e participe do sorteio de diárias na Vila do Comendador!
+            Ajude-nos a melhorar nossos serviços respondendo a pesquisa de satisfação, e participe do sorteio de diárias na Villa do Comendador!
           </p>
         </div>
 
         <div className={styles.block}>
           <h3 className={styles.h3}>1. DA PROMOÇÃO</h3>
           <p className={styles.text}>
-            O presente regulamento estabelece as condições para participação no
-            sorteio promocional promovido pela QUADRAIMOB SOLUÇÕES IMOBILIÁRIAS
-            LTDA, CNPJ/MF, empresa com sede no SCN, Quadra 05, Bloco A, Sala
-            817, Brasília Shopping – Torre Norte, Brasília-DF, inscrita no
-            CNPJ/MF sob nº 32.079.446/0001-40, neste ato representada por seus
-            sócios diretores Leonel Alves da Silva Netto e Rogerio de Oliveira
-            Silva, doravante designada simplesmente IMOBILIÁRIA, a qual sorteará
-            dentre os clientes adquirentes de imóveis comercializados por essa,
-            que tenham preenchido a pesquisa de satisfação, durante o período de
-            participação descrito nesse termo, e cumprimento de todos os
-            requisitos deste, conforme regras abaixo descritas.
+            O presente regulamento estabelece as condições para participação no sorteio
+            promocional promovido pela <b>QUADRAIMOB SOLUÇÕES IMOBILIÁRIAS
+            LTDA, CNPJ/MF</b>, empresa com sede no SCN, Quadra 05, Bloco A, Sala 817,
+            Brasília Shopping – Torre Norte, Brasília-DF, inscrita no CNPJ/MF sob nº
+            32.079.446/0001-40, neste ato representada por seus sócios diretores Leonel
+            Alves da Silva Netto e Rogerio de Oliveira Silva, doravante designada
+            simplesmente <b>IMOBILIÁRIA</b>, a qual sorteará dentre os clientes adquirentes de
+            imóveis comercializados por essa, que tenham preenchido a pesquisa de
+            satisfação, durante o período de participação descrito nesse termo, e
+            cumprimento de todos os requisitos deste, conforme regras abaixo descritas.
+
           </p>
         </div>
 
@@ -51,8 +50,8 @@ export function Regulamento() {
           <h3 className={styles.h3}>3. DO PERÍODO DE PARTICIPAÇÃO</h3>
           <p className={styles.text}>
             Participarão do sorteio todos os clientes que adquirirem os seus
-            imóveis no período compreendido entre o dia 01 de janeiro a 31 de
-            março, inclusive, não admitindo prorrogação do prazo, e desde que
+            imóveis <b>no período compreendido entre o dia 01 de janeiro a 31 de
+            março</b>, inclusive, não admitindo prorrogação do prazo, e desde que
             tenham preenchido a pesquisa de satisfação.
           </p>
         </div>
@@ -61,33 +60,52 @@ export function Regulamento() {
           <h3 className={styles.h3}>4. DOS REQUISITOS DE PARTICIPAÇÃO</h3>
 
           <p className={styles.text}>
-            4.1. Poderão participar do sorteio somente clientes que tenham
-            efetivamente concluído o contrato de compra e venda (assinatura
+            4.1. Poderão participar do sorteio <b>somente clientes que tenham
+            efetivamente concluído o contrato de compra e venda</b> (assinatura
             reconhecida/autenticada ou certificada) no período indicado no item
             3 acima, e que tenham preenchido a pesquisa de satisfação.
           </p>
 
           <p className={styles.text}>
-            4.2. A participação é restrita ao responsável pela compra, não sendo
+            4.2. A participação é <b>restrita ao responsável pela compra</b>, não sendo
             permitida a inscrição de terceiros, tampouco mediante procuração.
           </p>
 
           <p className={styles.text}>
-            4.3. Cada cliente poderá participar apenas uma única vez por unidade
-            compradora, independentemente da quantidade de imóveis adquiridos
-            nesse período com a Imobiliária.
+            4.3. Cada cliente poderá participar <b>uma vez para cada unidade</b> adquirida na QUADRAIMOB, no período informado no item 3 acima, desde que cumpridos todos os requisitos neste regulamento.
           </p>
 
           <p className={styles.text}>
-            4.4. A hospedagem objeto do sorteio é concedida a título de brinde
-            (mediante sorteio), exclusivamente para os clientes compradores que
-            responderem à pesquisa de satisfação de compra do imóvel, e somente
-            após o envio do formulário totalmente preenchido, será
-            disponibilizado e encaminhado pela IMOBILIÁRIA um link para
-            participação do sorteio com o respectivo Termo de Regulamento e o
-            qual deverá ser lido previamente e aceito todas as regras para
-            participação.
+            4.4. A hospedagem objeto do presente regulamento será concedida <b>a título de
+            brinde (mediante sorteio)</b>, exclusivamente para os clientes compradores que
+            aceitarem em participar da campanha <b>“PARTICIPE DA PESQUISA E
+            PARTICIPE DO SORTEIO”, respondendo à pesquisa de satisfação de
+            compra do imóvel</b>, e somente após o formulário totalmente preenchido,
+            automaticamente o cliente comprador terá acesso ao botão do regulamento, o
+            qual deverá ser lido previamente por esse com o posterior aceite de todas as
+            regras para participação do sorteio, a seguir:
           </p>
+          <ul className={styles.list}>
+            <li>O cliente receberá <u>o link da pesquisa de satisfação</u> e, automaticamente, terá
+acesso ao botão do <b>regulamento</b>, para leitura e <u>aceite das condições nele
+acordadas</u>;</li>
+            <li>
+              O sorteio ocorrerá no dia <b>01/04</b>, cujo resultado será divulgado na página do
+Instagram da quadraimob, e o cliente contemplado deverá <b>incluir data para
+retirada do voucher</b>, na sede do Plano Piloto, de segunda- a sexta-feira, em
+horário comercial, respeitando o período de retirada que deverá ocorrer entre 
+<b> 01/04 e 07/04</b> sem prorrogação, sendo a retirada de responsabilidade exclusiva
+do cliente contemplado;
+            </li>
+            <li>
+              Ressaltamos ainda que, o <b>agendamento das reservas é de responsabilidade
+              exclusiva do cliente contemplado</b>, devendo este contatar diretamente com a
+              central de reservas da <b>Villa do Comendador</b>, nos telefones: (62)3142-5866 ou
+              (62) 3142-7100, e-mail: <u>reserva@grupovillahoteis.com.br</u> seguindo os prazos
+              definidos por eles.
+            </li>
+            
+          </ul>
         </div>
 
         <div className={styles.block}>
@@ -99,18 +117,15 @@ export function Regulamento() {
 
           <ol className={styles.listAlpha}>
             <li>
-              Tenham realizado a compra do imóvel entre 01 de janeiro e 31 de
-              março de 2026 (inclusive);
+              tenham realizado a compra do imóvel entre <b>01 de janeiro e 31 de março de 2026</b>, (inclusive);
             </li>
             <li>
-              Sejam identificados como os responsáveis pela compra do imóvel;
+              seja(m) identificado(s) como o(s) responsável(is) pela compra do imóvel;
             </li>
             <li>
-              Respondam à pesquisa de satisfação disponibilizada após a
-              conclusão da compra do imóvel, dentro do prazo informado no item
-              acima.
+              responda(m) à pesquisa de satisfação disponibilizada, após a conclusão da compra do imóvel, dentro do prazo informado na alínea “a” acima.
             </li>
-            <li>Estejam com os dados atualizados;</li>
+            <li>estar(em) com os dados atualizados;</li>
           </ol>
 
           <p className={styles.text}>
@@ -122,12 +137,12 @@ export function Regulamento() {
         <div className={styles.block}>
           <h3 className={styles.h3}>6. DA APURAÇÃO E DO SORTEIO</h3>
           <p className={styles.text}>
-            6.1. A apuração do sorteio ocorrerá no dia 01 de abril de 2026.
+            6.1. A apuração do sorteio ocorrerá no dia <b>01 de abril de 2026</b>.
           </p>
           <p className={styles.text}>
-            6.2. O sorteio será realizado de forma online, por meio do perfil
-            oficial da QUADRAIMOB SOLUÇÕES IMOBILIÁRIAS LTDA., na rede social
-            Instagram, garantindo transparência e publicidade ao ato e
+            6.2. O sorteio será realizado de forma <b>online</b>, por meio do perfil
+            oficial da QUADRAIMOB SOLUÇÕES IMOBILIÁRIAS LTDA., na rede social 
+            <b> Instagram</b>, garantindo transparência e publicidade ao ato e
             posteriormente comunicado ao cliente.
           </p>
         </div>
@@ -135,16 +150,15 @@ export function Regulamento() {
         <div className={styles.block}>
           <h3 className={styles.h3}>7. DO PRÊMIO</h3>
           <p className={styles.text}>
-            7.1 O prêmio consistirá em 01 (uma) hospedagem, conforme as
-            seguintes condições:
+            7.1 O prêmio consistirá em um voucher com direito a <b> 01 (uma) hospedagem</b>, nas seguintes condições:
           </p>
           <ul className={styles.list}>
-            <li>02 (duas) diárias para casal;</li>
+            <li><b> 02 (duas) diárias para casal ou acompanhante</b>;</li>
             <li>
-              Utilização válida exclusivamente em período de baixa temporada;
+              Utilização válida <b> exclusivamente em período de baixa temporada</b>;
             </li>
             <li>
-              Hospedagem em Suíte Conforto Standard – Luxury Villa, contendo:
+              Hospedagem em <b> Suíte Conforto Standard – Luxury Villa</b>, contendo:
               <ul className={styles.list}>
                 <li>Cama King</li>
                 <li>TV LED HD </li>
@@ -159,7 +173,7 @@ export function Regulamento() {
             </li>
           </ul>
           <p className={styles.text}>
-            7.2 O prêmio é pessoal e intransferível, não podendo ser convertido
+            7.2 O prêmio é <b>pessoal e intransferível</b>, não podendo ser convertido
             em dinheiro, trocado ou substituído por outro benefício, a ser
             usufruído mediante reserva prévia e condições de disponibilidade do
             estabelecimento.
@@ -169,19 +183,19 @@ export function Regulamento() {
         <div className={styles.block}>
           <h3 className={styles.h3}>8. DA FORMA E DATA DE REALIZAÇÃO</h3>
           <p className={styles.text}>
-            8.1. O sorteio será realizado de forma online, no dia 01/04/2026,
+            8.1. O sorteio será realizado de forma <b>online</b>, no dia <b>01/04/2026</b>,
             por meio do perfil oficial da QUADRAIMOB SOLUÇÕES IMOBILIÁRIAS
-            LTDA., na rede social Instagram, na data prevista neste regulamento.
+            LTDA., na rede social <b>Instagram</b>, na data prevista neste regulamento.
           </p>
           <p className={styles.text}>
             8.2. Para fins de apuração, serão considerados todos os
             participantes que atendam integralmente aos critérios estabelecidos
             neste regulamento, sendo o contemplado escolhido de maneira
-            aleatória, dentre os participantes válidos.
+           <b> aleatória</b>, dentre os participantes válidos.
           </p>
 
           <p className={styles.text}>
-            8.3. A realização do sorteio poderá ser transmitida ao vivo (live)
+            8.3. A realização do sorteio poderá ser <b>transmitida ao vivo (live)</b>
             ou registrada por meio de vídeo, a exclusivo critério da QUADRAIMOB
             SOLUÇÕES IMOBILIÁRIAS LTDA garantindo transparência e publicidade ao
             procedimento e divulgação do resultado.
@@ -208,20 +222,21 @@ export function Regulamento() {
         <div className={styles.block}>
           <h3 className={styles.h3}>10. DA ENTREGA E UTILIZAÇÃO DO PRÊMIO</h3>
           <p className={styles.text}>
-            10.1. As condições para agendamento e utilização da hospedagem serão
-            informadas diretamente ao ganhador
+            10.1.  As condições para agendamento e utilização da hospedagem além das
+disponibilizadas no item 4 deste regulamento, serão informadas diretamente ao
+cliente contemplado.
           </p>
           <p className={styles.text}>
-            10.2. Eventuais despesas não expressamente incluídas no prêmio serão
-            de responsabilidade exclusiva do contemplado.
+            10.2. Eventuais despesas não expressamente incluídas no prêmio serão de
+responsabilidade exclusiva do cliente contemplado.
           </p>
         </div>
 
         <div className={styles.block}>
           <h3 className={styles.h3}>11. DAS DISPOSIÇÕES GERAIS</h3>
           <p className={styles.text}>
-            11.1. A participação neste sorteio implica na aceitação total e
-            irrestrita deste regulamento.
+            11.1. A participação neste sorteio implica na <b>aceitação total e
+            irrestrita deste regulamento</b>.
           </p>
           <p className={styles.text}>
             11.2. A QUADRAIMOB SOLUÇÕES IMOBILIÁRIAS LTDA reserva-se o direito
@@ -230,12 +245,12 @@ export function Regulamento() {
             fraudulento.
           </p>
           <p className={styles.text}>
-            11.3. Este sorteio possui caráter exclusivamente promocional, não
+            11.3. Este sorteio possui caráter <b>exclusivamente promocional</b>, não
             estando vinculado a pagamento adicional, sorte ou competição.
           </p>
 
           <p className={styles.text}>
-            11.4. Fica eleito o Foro da sede da IMOBILIÁRIA para dirimir
+            11.4. Fica eleito o Foro da sede da <b> IMOBILIÁRIA</b> para dirimir
             quaisquer dúvidas resultantes deste Termo.
           </p>
 
@@ -243,6 +258,10 @@ export function Regulamento() {
             E, por estarem assim, justas e concordes, as partes assinam o
             presente Termo de Regulamento em 02 (duas) vias de igual teor e
             forma, para que produzam os devidos e desejados efeitos.
+          </p>
+          
+          <p className={styles.textDate}>
+            Última atualização: 20 de fevereiro de 2026.             
           </p>
         </div>
       </div>
